6a67ba03d5247c55cc747d80465c7ba5dac6677b
Tenglong Li <litenglong0311@gmail.com>
2020-05-20 15:41:48 -0400

    simulation model 1
    Data simulation for model 1

---
+++ simulation/powersim.R
@@ -0,0 +1,179 @@
+#########################################
+## Simulation with powerful predictors ##
+#########################################
+
+rbinom <- Vectorize(rbinom,c("prob")) ## Vectorize the rbinom function for tb simulation
+
+## hhsim function is the function for simulating tb transmission for each household
+## Parameter explanations: 
+## data: the simulated data for each household
+## tpar: parameter values for the transmission model
+## beta: the parameter that defines "transmission force" based on the household size, i.e., the term (1/n)^beta in the transmission model, see Cauchemez et al. (2004)
+## apar: parameter values for the progression to disease model, right now there are only three: baseline, female and adult
+## activep: the probability of progressing to active disease 
+hhsim = function(data,tpar,beta,apar,pre_severe){
+  n <- nrow(data)
+  transmat <- mat.or.vec(n,n)
+  datemat <- mat.or.vec(n,n)
+  data$id <- 1:nrow(data)
+  lambdac <- tpar['theta']*(data$adult*tpar['adult']+(1-data$adult))*(data$female*tpar['female']+(1-data$female))*
+    (data$burden*tpar['burden']+(1-data$burden))*(data$ses*tpar['ses']+(1-data$ses))*(data$cough*tpar['cough']+(1-data$cough))
+  lambdaa <- apar['baseline']*(data$adult*apar['adult']+(1-data$adult))*(data$female*apar['female']+(1-data$female))
+  p <- 1-exp(-lambdac)
+  activep <- 1-exp(-lambdaa)
+  notb <- rbinom(1,1,prob = p)
+  if(n==1){
+    actb <- ifelse(notb>0,rbinom(1,1,prob = activep),0)
+    data$infect <- notb + actb
+    if (data$infect == 2){
+      data$date <- runif(1,0,10)
+    }
+  } else {
+    if (any(notb> 0)) {
+      actb <- numeric(n)
+      actb[which(notb > 0)] <- as.vector(rbinom(1,1,prob = activep[which(notb>0)]))
+      trans <- notb + actb
+    } else {
+      trans <- notb
+    }
+    date <- numeric(n)
+    diag(transmat) <- trans
+    if (any(trans == 2)){
+      date[which(trans==2)] <- runif(length(which(trans==2)),0,10)
+      diag(datemat) <- date
+      t <- min(date[date>0]) ## The diagnosing time of the sender, in this case it is the initial case
+      actind <- which(date==t)
+      data[tail(actind,n=1),]$severity <-  rbinom(1,1,prob = pre_severe)
+      ind <- 1:nrow(data)
+      sender <- tail(actind,n=1)
+      receiver <- ind[-actind]
+      while(length(actind) <= n){
+        crowd <- unique(data$crowd)
+        adult <- data[receiver,]$adult
+        female <- data[receiver,]$female
+        sleep <- data[receiver,]$sleep
+        time <- data[receiver,]$time
+        severity <- data[sender,]$severity
+        lambdah <- tpar['theta']*n^(beta)*(crowd*tpar['crowd']+(1-crowd))*(adult*tpar['adult']+(1-adult))*(female*tpar['female']+(1-female))*
+          (sleep*tpar['sleep']+(1-sleep))*(time*tpar['time']+(1-time))*(severity*tpar['severity']+(1-severity))
+        lambdaa <- apar['baseline']*(adult*apar['adult']+(1-adult))*(female*apar['female']+(1-female))
+        p <- 1-exp(-lambdah)
+        activep <- 1-exp(-lambdaa)
+        notb <- rbinom(1,1,p)
+        if (any(notb > 0)){
+          actb <- numeric(length(p))
+          actb[which(notb>0)] <- as.vector(rbinom(1,1,activep[which(notb>0)]))
+          trans <- actb+notb
+        } else {
+          trans <- notb
+        }
+        date <- numeric(length(p))
+        if (any(trans==2)){
+          date[which(trans==2)] <- t+rgamma(sum(trans==2),1.46,0.657)
+        } 
+        transmat[sender,receiver] <- trans
+        datemat[sender,receiver] <- date
+        if (all(trans < 2) & all(datemat[,-actind]==0)){break}
+        k <- datemat[,-actind]
+        t <- min(k[k>0])
+        sender <- which(datemat==t,arr.ind = TRUE)[,2]
+        actind <- c(actind,sender)
+        data[tail(actind,n=1),]$severity <-  rbinom(1,1,prob = pre_severe)
+        if (length(actind) == n) {break}
+        receiver <- ind[-actind]
+      }
+    }
+    data$infect <- apply(transmat,2,function(x) max(x,na.rm = TRUE))
+    data$date <- apply(datemat,2,function(x) ifelse(any(x>0),min(x[x>0]),NA))
+    data <- data%>%arrange(desc(infect),date)
+    transmat <- transmat[data$id,data$id]
+    transmat[lower.tri(transmat)] <- NA
+    com_noactive <- diag(transmat)[which(data$infect!=2)]
+    transmat[which(data$infect!=2),which(data$infect!=2)] <- NA
+    diag(transmat)[which(data$infect!=2)] <- com_noactive
+    data$edge <- apply(transmat,2,function(x) paste(as.character(x)[!is.na(as.character(x))],collapse = ""))
+  }
+  return(data)
+}
+
+hhsim <- compiler::cmpfun(hhsim)
+
+## tbsim is the function we will actually use
+## it basically do three things: 
+## First, it will generate simulated data for the entire population
+## Second, it will call hhsim to simulate tb transmission for each household
+## Third, it will do some postprocessing, which generate data for household contact study (using a sampler)
+## The parameter prevalence specifies the probability of crowdedness, adult, female, smear and culture in the simulation
+## The parameter munsize specifies the (stratified) sampling probabilities of all the communities 
+tbsim = function(hhno,hhmin,hhmean,tpar,beta,apar,prevalence,munsize){
+  library(tidyverse)
+  mypar <- tpar
+  mybeta <- beta
+  myactivep <- apar
+  names(mypar) <- c('theta','adult','female','crowd','sleep','time','severity','burden','ses','cough')
+  names(myactivep) <- c('baseline','adult','female')
+  names(prevalence) <- c('adult','female','crowd','sleep','time','severity','cough')
+  hhsize <- rpois(hhno, lambda = hhmean)
+  l <- length(which(hhsize<hhmin))
+  hhsize[hhsize<hhmin] <- sample(4:9,size = l,replace = TRUE)
+  hhid <- rep(1:hhno,hhsize)
+  crowd <- rep(sample(0:1,hhno,replace=TRUE,prob = c(1-prevalence['crowd'],prevalence['crowd'])),times=hhsize)
+  mun <- rep(sample(c('CARIACICA','SERRA','VITORIA','VILA VELHA'),hhno,replace=TRUE,prob = munsize),times=hhsize)
+  np <- sum(hhsize)
+  female <- rbinom(np,1,prob = prevalence['female'])
+  adult <- rbinom(np,1,prob = prevalence['adult'])
+  burden <- numeric(np)
+  burden[which(mun=='CARIACICA')] = 1;burden[which(mun=='SERRA')] = 0
+  burden[which(mun=='VITORIA')] = 0;burden[which(mun=='VILA VELHA')] = 1
+  ses <- numeric(np)
+  ses[which(mun=='CARIACICA')] = 0;ses[which(mun=='SERRA')] = 1
+  ses[which(mun=='VITORIA')] = 0;ses[which(mun=='VILA VELHA')] = 1
+  cough <- rbinom(np,1,prob = prevalence['cough'])
+  sleep <- rbinom(np,1,prob = prevalence['sleep'])
+  time <- rbinom(np,1,prob = prevalence['time'])
+  date <- rep(NA,np)
+  severity <- rep(NA,np)
+  dat <- cbind.data.frame(hhid,mun,crowd,female,adult,burden,ses,cough,sleep,time,severity,date,stringsAsFactors=FALSE)
+  names(dat) <- c('hhid','mun','crowd','female','adult','burden','ses','cough','sleep','time','severity','date')
+  dat <- split(dat,dat$hhid)
+  output <- lapply(dat,'hhsim',tpar=mypar,beta=mybeta,apar=myactivep,pre_severe = prevalence['severity'])
+  output <- do.call(rbind,output)
+  ## result <- output%>%group_by(hhid)%>%count(infect)%>%spread(key=infect,value=n,fill=0)
+  ## names(result) <- c('hhid','NoTB','LTB','ATB')
+  ## result <- data.frame(result)
+  hh1 <- output%>%group_by(hhid)%>%summarize(inf=sum(infect==2))%>%filter(inf>0)
+  hh1 <- hh1$hhid
+  output <- output%>%mutate(hhc=ifelse(hhid%in%hh1,1,0))
+  output <- output%>%group_by(hhid)%>%mutate(lag = as.numeric(date-min(date,na.rm=TRUE)))
+  output <- output%>%ungroup(hhid)
+  output <- output%>%arrange(hhid,desc(infect),lag)
+  output <- output%>%group_by(hhid)%>%mutate(nopar1=ifelse(is.na(lag),sum(!is.na(lag))+1,ifelse(lag==0,1,rank(lag,na.last = NA))),hhsize=n())
+  output <- output%>%mutate(nopar=ifelse(hhc==0,0,nopar1))
+  output <- output%>%mutate(nopar=ifelse(hhc==1&infect==0,0,nopar)) ## non-TB cases have 0 parameter
+  output <- output%>%mutate(nopar=ifelse(hhc==1&infect==2&nopar==1,0,nopar)) ## Initial active TB cases have 0 parameter
+  output <- output%>%ungroup(hhid)
+  output <- output%>%select(-c("date","id"))
+  return(output)
+}
+
+tbsim <- compiler::cmpfun(tbsim)
+
+## Parallel processing
+library(parallel)
+P <- detectCores(logical = FALSE) ## P = 4
+cl <- makeCluster(P)
+clusterEvalQ(cl, {rbinom <- Vectorize(rbinom,c("prob"));library(tidyverse);library(hhcsim)})
+clusterExport(cl,c("tbsim","hhsim"))
+plmcmc <- function(iter){
+  result <- tbsim(hhno=1500,hhmin=4,hhmean = 6.5,tpar=c(0.03,1.2,1,1.5,5.5,2,2.8,3.5,3,2.5),beta=0,apar=c(0.1,1,1),prevalence = c(0.7,0.5,0.4,0.1,0.3,0.5,0.2),munsize = rep(0.25,4))
+  dat <- result%>%filter(hhc==1)
+  mypar <- c(runif(1,0,1),exp(runif(9,0,1)))
+  m <- mcmc(dat,iter,mypar,0.1,0.2)
+  return(m)
+}
+
+out <- clusterApply(cl, rep(220000,250), fun = plmcmc)
+out1 <- lapply(out,function(x) x[seq(20001,220000,by=20),])
+saveRDS(out1,'powersim.rds')
+stopCluster(cl)
+
